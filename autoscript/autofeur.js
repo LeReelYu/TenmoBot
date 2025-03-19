fc832d7c2038568d057f5cb72e0f754cad9a0f48
LeReelYu <gatosama77@gmail.com>
2025-03-19 17:16:50 +0100

    correction virgule manquante

--- autoscript/autofeur.js
+++ autoscript/autofeur.js
@@ -2,8 +2,17 @@ module.exports = (client) => {
   client.on("messageCreate", (message) => {
     if (message.author.bot) return; // Ignore les messages des bots
 
-    // Si le message contient le mot "quoi", peu importe ce qui précède ou suit
-    if (message.content.toLowerCase().includes("quoi")) {
+    // Vérifie si le message contient un des mots-clés
+    if (
+      message.content.toLowerCase().includes("quoi") ||
+      message.content.toLowerCase().includes("koi") ||
+      message.content.toLowerCase().includes("kwa") ||
+      message.content.toLowerCase().includes("quo.i") ||
+      message.content.toLowerCase().includes("koa") ||
+      message.content.toLowerCase().includes("kuwa") ||
+      message.content.toLowerCase().includes("quooi") ||
+      message.content.toLowerCase().includes("quuoi")
+    ) {
       message.reply("feur");
     }
   });
