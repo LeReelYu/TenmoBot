1ed566b9338a68bdc8075009df0918c53087333a
LeReelYu <gatosama77@gmail.com>
2025-03-22 00:34:02 +0100

    amélioration de la vérification du bot

--- autoscript/bjorn.js
+++ autoscript/bjorn.js
@@ -27,18 +27,18 @@ module.exports = async function bjorn(client) {
           lastSentTime = currentTime; // Évite les doublons
 
           isPaused = true; // Active la pause
-          console.log("⏸️ Pause nez activée pour 30 minutes...");
+          console.log("⏸️ Pause nez activée pour 58 minutes...");
 
-          // Attendre 30 minutes avant de reprendre la vérification
+          // Attendre 58 minutes avant de reprendre la vérification
           setTimeout(() => {
             isPaused = false;
             console.log("▶️ Pause terminée, reprise de la vérification.");
-          }, 30 * 60 * 1000);
+          }, 3480000);
         } catch (error) {
           console.error("❌ Erreur lors de l'envoi du message :", error);
         }
       }
-    }, 1000); // Vérifie chaque seconde
+    }, 400); // Vérifie chaque milliseconde enfin bref tu comprends
   } catch (error) {
     console.error("❌ Erreur lors de la récupération du salon :", error);
   }
