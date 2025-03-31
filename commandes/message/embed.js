60e9432a5c12d77724f65e87d914c26e492a0dd5
LeReelYu <gatosama77@gmail.com>
2025-03-31 06:14:40 +0200

    correctif commande embed

--- commandes/message/embed.js
+++ commandes/message/embed.js
@@ -2,15 +2,18 @@ const {
   SlashCommandBuilder,
   EmbedBuilder,
   PermissionFlagsBits,
+  MessageFlags,
 } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("embed")
-    .setDescription("Envoie un embed prédéfini dans le salon.")
+    .setDescription("Envoie un embed prédéfini dans un salon spécifique.")
     .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers), // Nécessite la permission de bannir
 
   async execute(interaction) {
+    const channelId = "1332377646725857280"; // Remplace par l'ID du salon où tu veux envoyer l'embed
+
     const embed = new EmbedBuilder()
       .setAuthor({ name: "TEEMO'NEWS" })
       .setTitle("Récompense à venir")
@@ -49,6 +52,20 @@ module.exports = {
       .setColor("#0029f5")
       .setTimestamp();
 
-    await interaction.reply({ embeds: [embed] });
+    // Récupérer le salon avec l'ID et envoyer l'embed
+    const channel = await interaction.client.channels.fetch(channelId);
+
+    if (channel) {
+      await channel.send({ embeds: [embed] });
+      await interaction.reply({
+        content: "Embed envoyé dans le salon spécifié !",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "Salon introuvable.",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
   },
 };
