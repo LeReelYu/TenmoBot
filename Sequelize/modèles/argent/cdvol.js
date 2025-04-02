2328a9f466303f641a16ad5026d34431c919ee57
LeReelYu <gatosama77@gmail.com>
2025-04-02 15:23:07 +0200

    Nouveau modèle anciennement cooldowneco.js spécifique au vol

---
+++ Sequelize/modèles/argent/cdvol.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../sequelize"); // Ajustez ce chemin
+
+const Cdvol = sequelize.define("Cdvol", {
+  userId: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  lastAttempt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = Cdvol;
