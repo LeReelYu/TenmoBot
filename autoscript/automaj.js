d8a1ddc2175ca6df723fc184e4c7e407ef28f81d
LeReelYu <gatosama77@gmail.com>
2025-04-05 00:14:27 +0200

    adaptation du code à luxon pour voir

--- autoscript/automaj.js
+++ autoscript/automaj.js
@@ -1,22 +1,21 @@
+const { DateTime } = require("luxon");
+
 let lastCommitTime = null;
 
 function scheduleCommit() {
   setInterval(() => {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
+    const now = DateTime.now().setZone("Europe/Paris");
+    const hours = now.hour;
+    const minutes = now.minute;
 
-    // Si on est à 00:10 ou 12:10
-    if ((hours === 0 || hours === 12) && minutes === 10) {
-      // Vérifie si on n'a pas déjà commit dans cette même minute
+    if ((hours === 0 || hours === 12) && minutes === 20) {
       const currentKey = `${hours}:${minutes}`;
       if (lastCommitTime !== currentKey) {
         lastCommitTime = currentKey;
         commitDatabaseFile();
       }
     }
-  }, 30 * 1000); // Vérifie toutes les 30 secondes
+  }, 30 * 1000);
 }
 
-// Lancer la planification du commit
 scheduleCommit();
